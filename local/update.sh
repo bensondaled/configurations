e1404196d19ed42213b4d7953d6b6df0e06b3688
Ben Deverett <deverett@frontend.cluster>
2014-11-02 17:18:14 -0500

    added ipython for local

--- local/update.sh
+++ local/update.sh
@@ -4,3 +4,4 @@ cp ~/.profile ./profile
 cp -r ~/.custom_resources ./custom_resources
 cp -r ~/.custom_bin ./custom_bin
 cp -r ~/.ssh ./ssh
+cp -r ~/.ipython/profile_default/startup/ ./ipython_startup
