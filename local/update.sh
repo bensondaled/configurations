c9193688d5c5c954aff3a3d9ecfafa4f263dad8e
Ben <bendeverett@gmail.com>
2015-02-13 00:26:12 -0500

    more

--- local/update.sh
+++ local/update.sh
@@ -5,3 +5,4 @@ cp -r ~/.custom_resources ./custom_resources
 cp -r ~/.custom_bin ./custom_bin
 cp -r ~/.ssh ./ssh
 cp -r ~/.ipython/profile_default/startup/ ./ipython_startup
+cp -r /Users/Benson/Library/KeyBindings/DefaultKeyBinding.dict .
