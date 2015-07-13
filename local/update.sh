b60e1cef97d6ddce2c91ebb36d23c51124802cc9
Ben Deverett <bendeverett@gmail.com>
2015-07-13 10:09:21 -0400

    comment

--- local/update.sh
+++ local/update.sh
@@ -4,5 +4,6 @@ cp ~/.bash_profile ./bash_profile
 cp -r ~/.custom ./custom
 cp -r ~/.ssh ./ssh
 cp -r ~/.ipython/profile_default/startup/ ./ipython_startup
-cp -r /Users/Benson/Library/KeyBindings/DefaultKeyBinding.dict .
+cp -r ~/Library/KeyBindings/DefaultKeyBinding.dict .
 cp ~/.vimrc ./vimrc
+cp ~/.tmux.conf ./tmux.conf
