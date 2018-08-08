0eb82a4e8ee79ae5eacef00bf23ba709ef618b75
Ben Deverett <bendeverett@gmail.com>
2018-08-08 09:58:10 -0400

    more updates

--- local/update.sh
+++ local/update.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-cp ~/.bash_profile .
-cp -r ~/.custom .
+cp ~/.bash_profile ./bash_profile
+cp -r ~/.custom ./custom
 cp -r ~/.ipython/profile_default/startup/ ./ipython_startup
 cp -r ~/Library/KeyBindings/DefaultKeyBinding.dict .
-cp -r ~/.vim .
-cp ~/.vimrc .
-cp ~/.tmux.conf .
-cp -r ~/.matplotlib/matplotlibrc .
+cp -r ~/.vim ./vim
+cp ~/.vimrc ./vimrc
+cp ~/.tmux.conf ./tmux.conf
+cp -r ~/.matplotlib/matplotlibrc ./matplotlibrc
